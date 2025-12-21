f7b71b54e03981e86d987704532e32233adcb750
Mgfernandez1228 <Mgfernandez1228@outlook.com>
2025-12-20 23:38:30 -0500

    globalAtomScore is no set into kaplay.js

--- src/store.js
+++ src/store.js
@@ -6,3 +6,5 @@ export const isTextBoxVisibleAtom = atom(false);
 export const textBoxContentAtom = atom("");
 export const encounterAtom = atom(null); // null or string: 'level_one'|'level_two'|'level_three'
 export const store = createStore();
+
+export const scoreAtom = atom(0);
